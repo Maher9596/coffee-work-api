5ce12ab8f5198eeb6113c2163221a85b620ce240
Maher <muhran777@gmail.com>
2022-01-01 23:12:24 +0400

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 
-app.use('/posts', postRoutes)
+app.use('/', postRoutes)
 app.use('/', (req, res) => {
     res.send("We r up")
 })
