51c0042c5712648b899f5d973cfd8c487361bf21
Maher <muhran777@gmail.com>
2022-01-01 23:53:29 +0400

    app

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/routes.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 const PORT = process.env.PORT || 2000
-const CONNECTION = "mongodb+srv://admin:coffee&work@cluster0.mqbii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
@@ -15,6 +17,6 @@ app.use('/', (req, res) => {
     res.send("We r up")
 })
 
-mongoose.connect(CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)}))
 .catch((error) => console.log(error.message))
